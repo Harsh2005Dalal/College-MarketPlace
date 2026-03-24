83780dd117d6e75ce09e2833002dd05cbbbb4960
Harsh <harsh2005dalal@gmail.com>
2026-03-24 22:22:28 +0530

    Some error

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL || "http://localhost:5173", "http://localhost:5174"],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173", "http://localhost:5174", "https://college-market-place-opal.vercel.app"],
     credentials: true,
   })
 );
